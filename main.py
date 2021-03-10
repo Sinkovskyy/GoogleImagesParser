fcdd2be067e91e3c4ebd7696468048be7c015aea
Viktor <viktorsink202@gmail.com>
2021-03-10 11:34:53 +0200

    GoogleImagesParser now parser now working with paramentr ignore bad quality images

--- main.py
+++
@@ -1,7 +0,0 @@
-from googleImagesParser import GoogleImagesParser
-
-
-gip = GoogleImagesParser(headless = False,timeLimit=1.5)
-gip.download_images("son wallpaper",50,{3840,2160},ignore_bad_quality_img=True)
-
-
